498c1608372200dd1d894053d0b5d08e133edc0e
Vladimir <admin@ttp.by>
2022-08-24 17:04:44 +0300

    feat: Add style selector in levelselector

--- src/textbook/level-switcher/level-switcher.ts
+++ src/textbook/level-switcher/level-switcher.ts
@@ -3,7 +3,7 @@ import { BaseElement } from '../../utils/base-element/base-element';
 import { LevelButton } from '../level-button/level-button';
 import {
   IPublisher,
-  IPublisherLevelButton,
+  // IPublisherLevelButton,
   ISubscriber,
   ISubscriberLevelButton,
 } from '../textbook-interfaces';
@@ -12,24 +12,37 @@ import './level-switcher.scss';
 export class LevelSwitcher extends BaseElement<'div'>
   implements IPublisher, ISubscriberLevelButton {
 
-  private currentLevel: WordDifficultyGroup;
+  private currentLevel: LevelButton;
 
   private subscribers: ISubscriber[];
+
+  private levelButtons: LevelButton[];
 
   constructor() {
     super('div', 'textbook__level-switcher');
     const levelArr = Object.values(WordDifficultyGroup).sort();
-    this.currentLevel = levelArr[0];
     this.subscribers = [];
-    for (let i = 0; i < levelArr.length; i += 1) {
-      const button = new LevelButton(levelArr[i]);
+    this.levelButtons = levelArr.map((level) => {
+      const button = new LevelButton(level);
       button.register(this);
       this.element.append(button.element);
-    }
+      return button;
+    });
+    this.currentLevel = this.levelButtons[0];
+    this.currentLevel.element.classList.add('select');
+    // for (let i = 0; i < levelArr.length; i += 1) {
+    //   const button = new LevelButton(levelArr[i]);
+    //   button.register(this);
+
+    //   this.element.append(button.element);
+    // }
   }
 
-  update(publisher: IPublisherLevelButton): void {
-    this.currentLevel = publisher.level;
+  update(publisher: LevelButton): void {
+    this.currentLevel.element.classList.remove('select');
+    this.currentLevel = publisher;
+    this.currentLevel.element.classList.add('select');
+    // publisher.element.
     this.notify();
   }
 
@@ -46,6 +59,6 @@ export class LevelSwitcher extends BaseElement<'div'>
   }
 
   getCurrentLevel(): WordDifficultyGroup {
-    return this.currentLevel;
+    return this.currentLevel.level;
   }
 }
