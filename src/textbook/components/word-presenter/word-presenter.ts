fa95047be367d5016f4a58d34ebc4cfdf81a152b
Vladimir <admin@ttp.by>
2022-08-26 14:27:30 +0300

    feat: Add simple player

--- src/textbook/components/word-presenter/word-presenter.ts
+++ src/textbook/components/word-presenter/word-presenter.ts
@@ -4,13 +4,20 @@ import { IPublisherWordList, ISubscriber } from '../../models/textbook-interface
 import './word-presenter.scss';
 
 export class WordPresenter extends BaseComponent implements ISubscriber {
+  isPlaySound: boolean;
+
+  playlist: string[];
 
   constructor() {
     super('div', ['word-presenter']);
+    this.isPlaySound = false;
+    this.playlist = [];
   }
 
   update(pub: IPublisherWordList): void {
     const word = pub.currentCheckWord.word;
+    this.playlist.length = 0;
+    this.playlist.push(word.audio, word.audioExample, word.audioMeaning);
     const image = new BaseComponent('img', ['word-presenter__image']).element;
     image.setAttribute('src', `${BASE_URL}/${word.image}`);
     const description = new BaseComponent('div', ['word-presenter__description']).element;
@@ -71,7 +78,13 @@ export class WordPresenter extends BaseComponent implements ISubscriber {
     // eslint-disable-next-line max-len
     return isPlay ? '<?xml version="1.0" ?><svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" fill="darkgreen"><title/><g data-name="Layer 2" id="Layer_2"><path d="M19.45,4.11a1,1,0,0,0-1,.09L10.67,10H3a1,1,0,0,0-1,1V21a1,1,0,0,0,1,1h7.67l7.73,5.8A1,1,0,0,0,20,27V5A1,1,0,0,0,19.45,4.11Z"/><path d="M23,12a1,1,0,0,0-1,1v6a1,1,0,0,0,2,0V13A1,1,0,0,0,23,12Z"/><path d="M26,10a1,1,0,0,0-1,1V21a1,1,0,0,0,2,0V11A1,1,0,0,0,26,10Z"/><path d="M29,8a1,1,0,0,0-1,1V23a1,1,0,0,0,2,0V9A1,1,0,0,0,29,8Z"/></g></svg>'
     // eslint-disable-next-line max-len
-      : '<?xml version="1.0" ?><svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" fill="darkgrey"><title/><g data-name="Layer 2" id="Layer_2"><path d="M19.45,4.11a1,1,0,0,0-1,.09L10.67,10H3a1,1,0,0,0-1,1V21a1,1,0,0,0,1,1h7.67l7.73,5.8A1,1,0,0,0,20,27V5A1,1,0,0,0,19.45,4.11Z"/></g></svg>';
+      : '<?xml version="1.0" ?><svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" fill="#2907f5a1"><title/><g data-name="Layer 2" id="Layer_2"><path d="M19.45,4.11a1,1,0,0,0-1,.09L10.67,10H3a1,1,0,0,0-1,1V21a1,1,0,0,0,1,1h7.67l7.73,5.8A1,1,0,0,0,20,27V5A1,1,0,0,0,19.45,4.11Z"/></g></svg>';
+  }
+
+  private switchButtonAudioPlay(btn: HTMLButtonElement): void {
+    btn.innerHTML = this.getPlayIcon(!this.isPlaySound);
+    this.isPlaySound = !this.isPlaySound;
+    this.PlayArray(this.playlist);
   }
 
   private getButtonAudioPlay(): HTMLButtonElement {
@@ -79,7 +92,29 @@ export class WordPresenter extends BaseComponent implements ISubscriber {
       'button',
       ['word-presenter__audio-button'],
     ).element as HTMLButtonElement;
-    buttonAudio.innerHTML = this.getPlayIcon(false);
+    buttonAudio.innerHTML = this.getPlayIcon(this.isPlaySound);
+    buttonAudio.addEventListener('click', () => {
+      this.switchButtonAudioPlay(buttonAudio);
+    });
     return buttonAudio;
+  }
+
+  private PlayArray(urls: string[]) {
+    const player = new Audio();
+    const iteratorUrls = urls[Symbol.iterator]();
+    function setTrack() {
+      const currentTrack = iteratorUrls.next().value;
+      if (currentTrack) {
+        console.log(`${BASE_URL}/${currentTrack}`);
+        player.src = `${BASE_URL}/${currentTrack}`;
+        player.addEventListener('canplaythrough', () => {
+          player.play();
+        });
+        player.addEventListener('ended', () => {
+          setTrack();
+        });
+      }
+    }
+    setTrack();
   }
 }
