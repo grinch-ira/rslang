e9f6dab1b1e23dbe39368346ebf33393727fcee9
ruby -v <grinch.ira@gmail.com>
2022-08-28 14:23:45 +0300

    feat: adding start-page and login routing

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import { StartPage } from './start-page/components/start-page/start-page';
 import './reset.scss';
 import './style.scss';
 import './variables.scss';
-import { FormLogin } from './auth/components/form-login/form-login';
+import { App } from './routing/models/routing';
 
-
-window.onload = () => {
+/* window.onload = () => {
   document.body.appendChild(new StartPage().element);
   document.body.append(new FormLogin().element);
 };
+ */
+const app = new App();
+app.run();
--- src/start-page/components/start-page/start-page.ts
+++ src/start-page/components/start-page/start-page.ts
@@ -1,13 +1,11 @@
-import { HeaderWrapper } from '../header-wrapper/header-wrapper';
+
 import { MainWrapperImage } from '../main-wrapper-image/main-wrapper-image';
 import { FeaturesWrapper } from '../features-wrapper/features-wrapper';
 import { DevelopersWrapper } from '../developers-wrapper/developers-wrapper';
-import { Footer } from '../footer/footer';
 import './start-page.scss';
 import { BaseComponent } from '../../../shared/components/base-element/base-component';
 
 export class StartPage extends BaseComponent {
-  private headerWrapper: HeaderWrapper;
 
   private mainWrapperImage: MainWrapperImage;
 
@@ -15,19 +13,15 @@ export class StartPage extends BaseComponent {
 
   private developerWrapper: DevelopersWrapper;
 
-  private footer: Footer;
 
   constructor() {
     super('div', ['start-page']);
-    this.headerWrapper = new HeaderWrapper();
     this.mainWrapperImage = new MainWrapperImage();
     this.featuresWrapper = new FeaturesWrapper();
     this.developerWrapper = new DevelopersWrapper();
-    this.footer = new Footer();
-    this.element.append(this.headerWrapper.element);
     this.element.append(this.mainWrapperImage.element);
     this.element.append(this.featuresWrapper.element);
     this.element.append(this.developerWrapper.element);
-    this.element.append(this.footer.element);
+
   }
 }
