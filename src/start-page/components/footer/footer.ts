3cc24145a652e9d41115169ccab11878e9f5faab
Mldx <Anton.zaprutski@gmail.com>
2022-09-03 19:23:07 +0300

    feat: add button-base-element.ts

--- src/start-page/components/footer/footer.ts
+++ src/start-page/components/footer/footer.ts
@@ -1,6 +1,6 @@
 import { BaseComponent } from '../../../shared/components/base-element/base-component';
 // eslint-disable-next-line max-len
-import { ImageBaseElement } from '../../../shared/components/image-base-element.ts/image-base-element';
+import { ImageBaseElement } from '../../../shared/components/image-base-element/image-base-element';
 import './footer.scss';
 export class Footer extends BaseComponent {
   private footerGitHubLink: BaseComponent;
