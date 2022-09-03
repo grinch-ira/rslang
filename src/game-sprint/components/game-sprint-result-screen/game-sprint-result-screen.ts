4d11f509b418d312513374ccca787a67947173d0
Mldx <Anton.zaprutski@gmail.com>
2022-09-03 23:06:13 +0300

    feat: add audio in game sprint result

--- src/game-sprint/components/game-sprint-result-screen/game-sprint-result-screen.ts
+++ src/game-sprint/components/game-sprint-result-screen/game-sprint-result-screen.ts
@@ -3,10 +3,12 @@ import {
   ButtonBaseElement,
 } from '../../../shared/components/button-base-element/button-base-element';
 
-import { IWord } from '../../../api/api-interfaces';
+import { BASE_URL, IWord } from '../../../api/api-interfaces';
 import {
   GameSprintStartScreen,
 } from '../game-sprint-start-screen/game-sprint-start-screen';
+import { AudioPlayer } from '../../../shared/components/audio-player/audio-player';
+import './game-sprint-result-screen.scss';
 
 export class GameSprintResultScreen extends BaseComponent {
   element: HTMLDivElement;
@@ -21,32 +23,28 @@ export class GameSprintResultScreen extends BaseComponent {
   oneMoreGameButton =
     new ButtonBaseElement(['result-screen__one-more-game'], 'Сыграть ещё одну игру');
 
+  correctAnswerContainer = new BaseComponent(
+    'div',
+    ['result-screen__correct-answer'],
+    'Правильные ответы:',
+  );
+
+  incorrectAnswerContainer = new BaseComponent(
+    'div',
+    ['result-screen__incorrect-answer'],
+    'Неравильные ответы:',
+  );
+
   constructor(result: { correct: IWord[], mistake: IWord[] }) {
     super('div', ['game-sprint__result-screen']);
 
-    this.main.element.append(new BaseComponent(
-      'div',
-      ['result-screen__correct-answer-title'],
-      'Правильные ответы:').element,
+    this.main.element.append(
+      this.correctAnswerContainer.element,
+      this.incorrectAnswerContainer.element,
     );
 
-    result.correct.forEach(value => {
-      this.main.element.append(
-        new BaseComponent('div', [], `${value.word} - ${value.wordTranslate}`).element,
-      );
-    });
-
-    this.main.element.append(new BaseComponent(
-      'div',
-      ['result-screen__incorrect-answer-title'],
-      'Неправильные ответы:').element,
-    );
-
-    result.mistake.forEach(value => {
-      this.main.element.append(
-        new BaseComponent('div', [], `${value.word} - ${value.wordTranslate}`).element,
-      );
-    });
+    this.addWordToContainer(result.correct, this.correctAnswerContainer.element);
+    this.addWordToContainer(result.mistake, this.incorrectAnswerContainer.element);
 
     this.element.append(
       this.title.element,
@@ -61,6 +59,23 @@ export class GameSprintResultScreen extends BaseComponent {
 
     this.textbookButton.element.addEventListener('click', () => {
       window.location.hash = '#textbook';
+    });
+  }
+
+  addWordToContainer(arrayWord: IWord[], container: HTMLElement) {
+    arrayWord.forEach(value => {
+      const player = new AudioPlayer();
+      player.addInPlaylist(`${BASE_URL}/${value.audio}`);
+      const btn = new ButtonBaseElement(['word-audio']).element;
+      player.setControlElement(btn);
+      const div = new BaseComponent(
+        'div',
+        ['word-container'],
+        `${value.word} - ${value.wordTranslate}`,
+      ).element;
+      div.prepend(btn);
+
+      container.append(div);
     });
   }
 }
